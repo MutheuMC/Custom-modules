de35c25197268f2afd9bc0808c5e9c12497ac5d0
DekutSiemens <smscp.kenya@dkut.ac.ke>
2025-10-14 09:37:35 +0300

    Added new functionality

---
+++ equipment_management/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from . import equipment_category
+from . import equipment_location
+from . import equipment_item
+from . import equipment_loan
+from . import equipment_maintenance
