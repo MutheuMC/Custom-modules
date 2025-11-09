3a94e2f07e4461f9fcfd90576258747604705180
DekutSiemens <smscp.kenya@dkut.ac.ke>
2025-11-09 07:32:11 -0800

    delete unrelated files

--- custom_docs/models/res_company.py
+++
@@ -1,9 +0,0 @@
-# custom_documents/models/res_company.py
-from odoo import fields, models
-
-class ResCompany(models.Model):
-    _inherit = 'res.company'
-    
-    documents_folder_id = fields.Many2one('documents.folder', string='Documents Folder',
-                                          help='Main folder for company documents')
-    documents_tags_ids = fields.Many2many('documents.tag', string='Document Tags')
--- custom_docs/models/res_users.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-class ResUsers(models.Model):
-    _inherit = 'res.users'
-    
-    document_ids = fields.One2many('documents.document', 'owner_id', string='Documents')
-
-
